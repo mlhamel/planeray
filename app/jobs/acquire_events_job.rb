c6a6fa1b5552b845b571720918442429720fc63d
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2016-09-07 22:10:31 -0400

    Cleans the acquisition job

--- app/jobs/acquire_events_job.rb
+++ app/jobs/acquire_events_job.rb
@@ -1,18 +1,17 @@
 class AcquireEventsJob < BaseJob
   def perform
+    items = client.latest_items
+
     puts "Connecting to rtl_sdr..."
-    puts "found #{latest_items.count}"
-    latest_items.map do |item|
+    puts "found #{items.count}"
+
+    items.map do |item|
       event = Event.create!(**item.symbolize_keys)
       puts "Created a new event for #{event.flight}"
     end
   end
 
   private
-
-  def latest_items
-    client.latest_items
-  end
 
   def client
     @client ||= Dump1090::Client.new
